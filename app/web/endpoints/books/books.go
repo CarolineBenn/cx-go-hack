bcec5a64f1775d1966e694863009a0244ba2d340
Caroline Benn <lineybenn@gmail.com>
2018-07-27 14:46:31 +0100

    Create serie route

--- app/web/endpoints/books/books.go
+++ app/web/endpoints/books/books.go
@@ -1,4 +1,4 @@
-package stats
+package books
 
 import (
 	"encoding/json"
@@ -29,17 +29,40 @@ type Book struct {
 	Title       string `json:"title"`
 	ReleaseDate string `json:"releaseDate"`
 	Position    int    `json:"position"`
+	Bought      bool   `json:"bought"`
+	SeriesTitle string
 }
 
-// Books func
-func Books(c *gin.Context) {
+// All func
+func All(c *gin.Context) {
+	series := getData(c)
 
+	c.JSON(200, gin.H{"Books": series})
+}
+
+// Latest func
+func Latest(c *gin.Context) {
+	series := getData(c)
+
+	response := Serie{}
+	for i := 0; i < len(series.Series); i++ {
+		end := len(series.Series[i].Books) - 1
+		book := series.Series[i].Books[end]
+		book.SeriesTitle = series.Series[i].Title
+		response.Books = append(response.Books, book)
+	}
+
+	c.JSON(200, gin.H{"Latest Books": response.Books})
+}
+
+// getData func
+func getData(c *gin.Context) Series {
 	// Open our jsonFile
 	jsonFile, err := os.Open("app/data/index.json")
 
 	if err != nil {
 		fmt.Println(err)
-		c.JSON(200, gin.H{"books": "false"})
+		c.JSON(200, gin.H{"error": "Couldn't find any books"})
 	}
 
 	defer jsonFile.Close()
@@ -50,14 +73,30 @@ func Books(c *gin.Context) {
 
 	json.Unmarshal(byteValue, &series)
 
-	for i := 0; i < len(series.Series); i++ {
-		fmt.Println(series.Series[i].Title + " by " + series.Series[i].Author + " (" , len(series.Series[i].Books) , "books in series)")
-	}
-
-	c.JSON(200, gin.H{"series": series.Series})
+	return series
 }
 
+// ToBuy func
+func ToBuy(c *gin.Context) {
+	series := getData(c)
 
+	response := Series{}
+	for i := 0; i < len(series.Series); i++ {
+		group := series.Series[i]
+		group.Books = nil
+		response.Series = append(response.Series, group)
+		for j := 0; j < len(series.Series[i].Books); j++ {
+			if !series.Series[i].Books[j].Bought {
+				book := series.Series[i].Books[j]
+				response.Series[i].Books = append(response.Series[i].Books, book)
+			}
+		}
+	}
+
+	c.JSON(200, gin.H{"Books to buy": response})
+}
+
+// Show an individual series
 func BookRoute(c *gin.Context) {
 	// Open our jsonFile
 	jsonFile, err := os.Open("app/data/index.json")
@@ -92,4 +131,3 @@ func BookRoute(c *gin.Context) {
 		c.JSON(200, gin.H{"error":"404 – Not found"})
 	}
 }
-
--- app/web/router.go
+++ app/web/router.go
@@ -2,16 +2,23 @@ package web
 
 import (
 	"github.com/CarolineBenn/cx-go-hack/app/web/endpoints"
-	"github.com/CarolineBenn/cx-go-hack/app/web/endpoints/stats"
+	"github.com/CarolineBenn/cx-go-hack/app/web/endpoints/books"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
+	// Health
 	r.GET("/health", endpoints.Health)
-  r.GET("/books", stats.Books)
-	r.GET("/books/:id", stats.BookRoute) // id is title-with-hyphens
+
+	// Books
+	r.GET("/books/latest", books.Latest)
+	r.GET("/books/toBuy", books.ToBuy)
+
+	// Whole series
+	r.GET("/series/:id", books.BookRoute) // id is title-with-hyphens
+	r.GET("/series", books.All)
 
 	return r
 }
